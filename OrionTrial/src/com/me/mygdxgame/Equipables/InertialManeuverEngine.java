7583531245f65cb2a3d1e0611708167a7edb09ca
ryanshowseason2 <ryanshowseason2@gmail.com>
2014-06-15 23:11:20 -0400

    inertial manuever animation

--- OrionTrial/src/com/me/mygdxgame/Equipables/InertialManeuverEngine.java
+++ OrionTrial/src/com/me/mygdxgame/Equipables/InertialManeuverEngine.java
@@ -1,5 +1,7 @@
 package com.me.mygdxgame.Equipables;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.me.mygdxgame.Entities.Ship;
@@ -12,11 +14,19 @@ public class InertialManeuverEngine extends ManeuverEngine
 	float m_xVelocity = 0;
 	float m_yVelocity = 0;
 	int m_dodgeCounter = 30;
+	float m_xdelta = 0f;
+	float m_ydelta = 0f;
+	double m_angleToEmitAt = 0f;
 	
 	public InertialManeuverEngine(Ship s, float maxVelocity)
 	{
 		super(s, maxVelocity);
 		m_boostMagnitude = m_maxVelocity;
+		
+		m_engineEffect.load(Gdx.files.internal("data/starslingwave.p"), Gdx.files.internal("data/"));
+		m_engineEffectPool = new ParticleEffectPool(m_engineEffect, 1, 2);
+		m_pooledEngineEffect = m_engineEffectPool.obtain();
+		m_pooledEngineEffect.allowCompletion();
 	}
 	
 	public void ApplyVelocity()
@@ -27,7 +37,6 @@ public class InertialManeuverEngine extends ManeuverEngine
 		
 		m_ship.m_body.setLinearVelocity(m_xVelocity, m_yVelocity);
 		m_dodgeCounter = 30;
-		m_boostJuice -= 1;	
 	}
 
 	@Override
@@ -38,7 +47,17 @@ public class InertialManeuverEngine extends ManeuverEngine
 			m_xVelocity = (float)(m_boostMagnitude * Math.cos(m_ship.m_angleRadians));
 			m_yVelocity = (float)(m_boostMagnitude * Math.sin(m_ship.m_angleRadians));
 	        
-	        ApplyVelocity();
+	        ApplyVelocity();	
+	        m_pooledEngineEffect.reset();
+	        
+	        m_pooledEngineEffect.getEmitters().get(0).getAngle().setLow(m_ship.m_angleDegrees);
+	        m_pooledEngineEffect.getEmitters().get(1).getAngle().setLow(m_ship.m_angleDegrees);
+	        m_pooledEngineEffect.getEmitters().get(0).getRotation().setLow(m_ship.m_angleDegrees);	   
+	        m_pooledEngineEffect.getEmitters().get(1).getRotation().setLow(m_ship.m_angleDegrees);
+	        m_angleToEmitAt = m_ship.m_angleDegrees;
+	        float radius = 150;
+	        m_xdelta =  (float)(radius * Math.cos(m_ship.m_angleRadians));
+	        m_ydelta =  (float)(radius * Math.sin(m_ship.m_angleRadians));
 		}
 	}
 
@@ -51,6 +70,15 @@ public class InertialManeuverEngine extends ManeuverEngine
 			m_yVelocity = (float)(-m_boostMagnitude * Math.sin(m_ship.m_angleRadians));
 	        	        
 	        ApplyVelocity();
+	        m_pooledEngineEffect.reset();
+	        m_pooledEngineEffect.getEmitters().get(0).getAngle().setLow(m_ship.m_angleDegrees - 180);
+	        m_pooledEngineEffect.getEmitters().get(1).getAngle().setLow(m_ship.m_angleDegrees - 180);
+	        m_pooledEngineEffect.getEmitters().get(0).getRotation().setLow(m_ship.m_angleDegrees - 180);
+	        m_pooledEngineEffect.getEmitters().get(1).getRotation().setLow(m_ship.m_angleDegrees - 180);
+
+	        float radius = 150;
+	        m_xdelta = -(float)(radius * Math.cos(m_ship.m_angleRadians));
+	        m_ydelta = -(float)(radius * Math.sin(m_ship.m_angleRadians));
 		}
 	}
 
@@ -63,7 +91,16 @@ public class InertialManeuverEngine extends ManeuverEngine
 			m_yVelocity = (float)(m_boostMagnitude * Math.cos(m_ship.m_angleRadians));
 	        
 	        
-	        ApplyVelocity();
+	        ApplyVelocity();	
+	        m_pooledEngineEffect.reset();
+	        
+	        m_pooledEngineEffect.getEmitters().get(0).getAngle().setLow(m_ship.m_angleDegrees + 90);
+	        m_pooledEngineEffect.getEmitters().get(1).getAngle().setLow(m_ship.m_angleDegrees + 90);
+	        m_pooledEngineEffect.getEmitters().get(0).getRotation().setLow(m_ship.m_angleDegrees + 90);
+	        m_pooledEngineEffect.getEmitters().get(1).getRotation().setLow(m_ship.m_angleDegrees + 90);
+	        float radius = 150;
+	        m_xdelta = (float)(radius * Math.cos(m_ship.m_angleRadians+Math.PI/2));
+	        m_ydelta = (float)(radius * Math.sin(m_ship.m_angleRadians+Math.PI/2));
 		}
 	}
 
@@ -76,6 +113,15 @@ public class InertialManeuverEngine extends ManeuverEngine
 			m_yVelocity = (float)(-m_boostMagnitude * Math.cos(m_ship.m_angleRadians));
 	        
 	        ApplyVelocity();
+	        m_pooledEngineEffect.reset();
+	        
+	        m_pooledEngineEffect.getEmitters().get(0).getAngle().setLow(m_ship.m_angleDegrees - 90);
+	        m_pooledEngineEffect.getEmitters().get(1).getAngle().setLow(m_ship.m_angleDegrees - 90);
+	        m_pooledEngineEffect.getEmitters().get(0).getRotation().setLow(m_ship.m_angleDegrees - 90);
+	        m_pooledEngineEffect.getEmitters().get(1).getRotation().setLow(m_ship.m_angleDegrees - 90);
+	        float radius = 150;
+	        m_xdelta = -(float)(radius * Math.cos(m_ship.m_angleRadians+Math.PI/2));
+	        m_ydelta = -(float)(radius * Math.sin(m_ship.m_angleRadians+Math.PI/2));
 		}
 	}
 
@@ -92,13 +138,22 @@ public class InertialManeuverEngine extends ManeuverEngine
 			{
 				m_dodgeCounter = 0;
 			}
+			
+			
 		}
 		
 		if( m_dodging && m_dodgeCounter <= 0 )
 		{
 			m_ship.m_body.setLinearVelocity(m_origxVelocity, m_origyVelocity );
 			m_dodging = false;
-		}
+			m_pooledEngineEffect.allowCompletion();
+		}		
+
+		{
+			m_pooledEngineEffect.setPosition(m_ship.m_objectXPosition+ m_xdelta, m_ship.m_objectYPosition + m_ydelta);
+			m_pooledEngineEffect.draw(renderer, 10f/60f);
+		}		
+		
 	}
 
 	@Override
--- OrionTrial/src/com/me/mygdxgame/Equipables/ManeuverEngine.java
+++ OrionTrial/src/com/me/mygdxgame/Equipables/ManeuverEngine.java
@@ -1,6 +1,9 @@
 package com.me.mygdxgame.Equipables;
 
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
 import com.me.mygdxgame.Entities.Ship;
 
 public abstract class ManeuverEngine 
@@ -9,6 +12,9 @@ public abstract class ManeuverEngine
 	public float m_maxVelocity;
 	public float m_boostJuice = 100;
 	public float m_boostMagnitude = 15000f;
+	ParticleEffect m_engineEffect = new ParticleEffect();
+    ParticleEffectPool m_engineEffectPool;
+    PooledEffect m_pooledEngineEffect;
 	public ManeuverEngine( Ship s, float maxVelocity )
 	{
 		m_ship = s;
